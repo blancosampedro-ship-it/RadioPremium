3a42b81a5fe59afceec899e0304c10b09f816b96
jjbs <twetter@gmail.com>
2026-04-27 20:51:00 +0200

    Initial Commit

---
+++ RadioPremium/RadioPremiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioPremiumApp.swift
+//  RadioPremium
+//
+//  Created by Winfin Sistemas on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct RadioPremiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
